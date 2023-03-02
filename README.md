2fe56a3888aff9c36971eb055a9649dccea1e642
Binij Shrestha <shresthabinij@gmail.com>
2023-03-02 21:30:58 +0545

    working demo of expres server

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ## Scale to your api container into as many instance needed
 
 > docker-compose up --scale api=10
+
+## Test
+
+> curl http://localhost:3000
+> curl -I http://localhost:3000
--- express-app/index.js
+++ express-app/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const os = require('os');
 
 const app = express()
+const port = process.env.PORT || 3000;
 
 app.use('/', async (req, res) => {
     console.log(`I am sending a response ${os.hostname()}`)
-    res.setHeader()
+
     res.json({ message: 'Ok it works...', hostname: os.hostname() })
+
 })
 
-app.listen(3000, () => console.log('Listening server on port 3000'))
+app.listen(port, () => console.log(`Listening server on ${port}`))
